06e79c64cbdb9f265623468e9a33a497254228ad
Sonal <nkscoder@gmail.com>
2020-04-01 18:33:18 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+"use strict";
+
+$(document).ready(function(){
+
+// mobile_menu
+var menu = $('ul#navigation');
+if(menu.length){
+	menu.slicknav({
+		prependTo: ".mobile_menu",
+		closedSymbol: '+',
+		openedSymbol:'-'
+	});
+};
+});
+
+})(jQuery);

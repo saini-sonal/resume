59ef056984fe0138945388ae4f6ce647544d4500
sonal <sonal@scaledesk.xyz>
2020-04-08 12:33:33 +0530

    make changes

--- js/main.js
+++ js/main.js
@@ -3,15 +3,29 @@
 
 $(document).ready(function(){
 
-// mobile_menu
-var menu = $('ul#navigation');
-if(menu.length){
-	menu.slicknav({
-		prependTo: ".mobile_menu",
-		closedSymbol: '+',
-		openedSymbol:'-'
-	});
-};
-});
 
+
+ $(window).scroll(function(){
+       $('nav').toggleClass('scrolled',$(this).scrollTop()>350);
+    });
+
+
+$(document).scroll(function() {
+var scrollDistance = $(this).scrollTop();
+if (scrollDistance > 100) {
+$('.scroll-to-top').fadeIn();
+} else {
+$('.scroll-to-top').fadeOut();
+}
+});
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+});
 })(jQuery);
